936e0623c3e7b0bf5dd93df5786ac68f6687da86
KARTHIK S P <spkarthik1211@gmail.com>
2026-05-26 16:17:47 +0530

    Update vite.config.js

--- Frontend/client/vite.config.js
+++ Frontend/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/graphql": "https://site-network.onrender.com/",
+      "/graphql": "https://site-network.onrender.com",
     },
   },
 });
